d4af4d2531c2e4f4653267dca85744181a7f2ed2
Zach David <112095798+6ZachDavid9@users.noreply.github.com>
2022-11-26 10:39:25 +0000

    changed into a do while nested loop

--- maregmen_lab_activity3.c
+++ maregmen_lab_activity3.c
@@ -1,47 +1,69 @@
 #include<stdio.h>
 
-int odd_numbers();
-int even_numbers();
+int odd_numbers();   // we initialize two functions
+int even_numbers();  // to have a cleaner code
 
 int main(void) {
 
     int first, second, cond;
+
+    // i will use nested do while loops for this problem. 
+    // it will ask the user to input to integers and it will repeat the task until the condition becomes false
+    // I first came up with two non-nested do while loops.
+    // we can do two do while loops without nesting them and it will still come up with the same output.
+    // but as a programmer, I'd like to experiment
+    // this problem could be solved using other loops but for me this is a cleaner code to read.
+    
     do {
-        printf("Enter first number: "); scanf("%d", &first);
-        printf("Enter second number: "); scanf("%d", &second);
-        printf("Enter [1] to use display even [2] to display odd: "); scanf("%d", &cond);
-        
-        if (first > second) {
-            printf("Invalid Input Range.");
+        printf("Enter a number: ");
+        scanf("%d", &first);
+        printf("Enter another number: ");
+        scanf("%d", &second);
+
+        if (first >= second) {
+            printf("Invalid input Range.\n");
             continue;
         }
-        if (cond > 2) {
-            printf("Invalid Input");
-            continue;
-        }
-        break;
+        // I nested 
+        do {
+            
+            printf("Enter [1] to use display even [2] to display odd: ");
+            scanf("%d", &cond);
 
-    } while (second > first && cond < 3);
+            if (cond > 2) {
+                printf("Invalid input!\n");
+                continue;
+            }
 
-    if (cond == 1) { 
-        printf("h");
-        even_numbers(first, second);
-    } else if (cond == 2) {
+        } while(cond > 2);
+
+
+    } while (first >= second);
+    
+    
+    if (cond==1) {
         odd_numbers(first, second);
+    } else {
+        even_numbers(first, second); 
     }
+    printf("\n");
 
 }
 
+// odd_numbers funciton, it will return the odd numbers BETWEEEN the first and second integer argument
 int odd_numbers(int first, int second) {
-    for(int i = 1; first <= second; first += i) {
+    printf("Odd numbers from %d to %d.\n", first, second);
+    for(first++; first < second-1; first++) { // it will intialize a statement which is to add 1, in order to skip the number so it cannot proceed to the if condition.
         if ((first%2)) {
             printf("%d ", first);
         }
     }
 }
 
+// even_numbers funciton, it will return the even numbers BETWEEEN the first and second integer argument
 int even_numbers(int first, int second) {
-    for(int i = 1; first <= second; first += i) {
+    printf("Odd numbers from %d to %d.\n", first, second); 
+    for(first++; first <= second; first++) { // it will intialize a statement which is to add 1, in order to skip the number so it cannot proceed to the if condition.
         if (!(first%2)) {
             printf("%d ", first);
         }
