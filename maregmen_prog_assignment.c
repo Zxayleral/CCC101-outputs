db0ed6f614af6e7f65402abec4f63ebcb81cc03f
Zach David <112095798+6ZachDavid9@users.noreply.github.com>
2023-01-17 01:49:55 +0000

    Finished at 'date'

--- maregmen_prog_assignment.c
+++ maregmen_prog_assignment.c
@@ -40,10 +40,13 @@ void generatePassword(int len) {
     char password[len];
     for(int i = 0; i < len; i++) {
         int ch = randInt(1, 4); // this variable will contain a number between 1 and 4, and we can use it for randomizing what type of character to create
+        
+        // we could use a variable containing all the characters including symbols,n numbers and letters, but the probabllity of choosing 1 of the 4 types would be low.
         if (ch==1) password[i] = smol_letters[randInt(0, 25)];
         if (ch==2) password[i] = big_letters[randInt(0, 25)];
         if (ch==3) password[i] = supported_symbols[randInt(0, 20)];
         if (ch==4) password[i] = numbers[randInt(0, 9)];
+
     } 
     printf("Password generated %s \n", password);
 }
